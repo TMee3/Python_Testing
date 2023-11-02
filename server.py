961a71b61ad965fa899ec64cc7b2210a51ea823d
TMee3 <theopsna@protonmail.com>
2023-11-02 01:54:18 +0100

    fix

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import json
-from flask import Flask,render_template,request,redirect,flash,url_for
+
+from flask import Flask, render_template, request, redirect, flash, url_for
 
 
 def loadClubs():
@@ -20,14 +21,19 @@ app.secret_key = 'something_special'
 competitions = loadCompetitions()
 clubs = loadClubs()
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/showSummary',methods=['POST'])
+
+@app.route(rule='/showSummary', methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    club = [club for club in clubs if club['email'] == request.form['email']]
+    if len(club) > 0:
+        return render_template(template_name_or_list='welcome.html', club=club[0], competitions=competitions)
+    else:
+        return render_template(template_name_or_list='index.html', error="Sorry, that email wasn't found.")
 
 
 @app.route('/book/<competition>/<club>')
