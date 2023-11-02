c95aeac31a4747268b688d7f915b0579c0e4c4fc
TMee3 <theopsna@protonmail.com>
2023-11-02 02:41:27 +0100

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from flask import Flask, render_template, request, redirect, flash, url_for
 
 
+
 def loadClubs():
     with open('clubs.json') as c:
          listOfClubs = json.load(c)['clubs']
